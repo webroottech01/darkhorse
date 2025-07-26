53ad007fdb50a416a3541353f6276a508c93cae6
Khushal024 <khushalranka@gmail.com>
2025-07-26 13:14:18 +0530

    new codes updated

--- next.config.js
+++ next.config.js
@@ -1,23 +1,27 @@
-const path = require('path')
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/stagingapp',
+  assetPrefix: '/stagingapp/',
+
   images: {
     domains: ['assets.dispenseapp.com', 'dispense-images.imgix.net'],
     loader: 'custom',
     loaderFile: './src/imageLoader.js',
     formats: ['image/avif', 'image/webp'],
   },
+
   compiler: {
     styledComponents: true,
   },
+
   webpack: (config) => {
     config.resolve.alias['styled-components'] = path.resolve(
       './node_modules/styled-components'
-    )
-
-    return config
+    );
+    return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig; 
